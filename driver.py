75787dc537f9a22158d25b28b666816c3fab2f9a
Kelvin Davis <273degreeskelvin@gmail.com>
2021-03-15 21:21:25 +1100

    Added driver function to simplify to cli args

---
+++ driver.py
@@ -0,0 +1,32 @@
+"""
+The program that drives the finding of solutions for Letters and Numbers.
+
+Usage: `python driver.py 3 4 5 6 7 8 80`
+The last number is treated as the target and all numbers prior are part of the
+expression formed by the solution.
+"""
+
+import sys
+import subprocess
+
+if len(sys.argv) < 3:
+    raise ValueError("Not enough args given")
+
+NUMBERS = [int(x) for x in sys.argv[1:-1]]
+TARGET = int(sys.argv[-1])
+
+proc_minizinc = subprocess.Popen([
+    'minizinc',
+    'lettersAndNumbers.mzn',
+    '-D', f'n = {len(NUMBERS)}; numbers = {NUMBERS}; target = {TARGET}',
+    '--output-mode', 'json',
+    '--all-solutions',
+], stdout=subprocess.PIPE)
+
+proc_parser = subprocess.Popen([
+    'python',
+    'letters_and_numbers.py',
+], stdin=proc_minizinc.stdout, stdout=subprocess.PIPE)
+
+for line in proc_parser.stdout:
+    print(line.decode('utf-8').strip())
